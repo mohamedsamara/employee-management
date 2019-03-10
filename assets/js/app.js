37903904080f7b6bb23ada2b0855b4d07c5cae6d
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-03-10 00:11:15 -0800

    fetch & save data from/to firebase database

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,46 @@
 $(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyCUOpfYe9sBD7_e30Cg_-CrPb0iaLJOW4U',
+    authDomain: 'employee-management-5bdc0.firebaseapp.com',
+    databaseURL: 'https://employee-management-5bdc0.firebaseio.com',
+    projectId: 'employee-management-5bdc0',
+    storageBucket: 'employee-management-5bdc0.appspot.com',
+    messagingSenderId: '243267298051'
+  };
+  firebase.initializeApp(config);
+
+  // load data into employee table
+  var getEmployees = function() {
+    firebase
+      .database()
+      .ref('/employee/')
+      .once('value')
+      .then(function(snapshot) {
+        $.each(snapshot.val().new, function(i, value) {
+          var markup =
+            '<tr><td>' +
+            value.name +
+            '</td><td>' +
+            value.role +
+            '</td><td>' +
+            value.start_date +
+            '</td><td>' +
+            value.months_worked +
+            '</td><td>' +
+            value.rate +
+            '</td><td>' +
+            value.total_bill +
+            ' $' +
+            '</td></tr>';
+
+          $('.employee-table tbody').append(markup);
+        });
+      });
+  };
+
+  getEmployees();
+
   // add employee row to employee table
   $('#add-employee').on('submit', function(e) {
     e.preventDefault();
@@ -11,6 +53,30 @@ $(document).ready(function() {
     var timeNow = new Date();
     var monthsWorked = getMonthsDifference(formattedDate, timeNow);
     var totalBill = getTotalBill(monthsWorked, rate);
+
+    // construct an object ready to be sent to database
+    var newEmployee = {
+      name: name,
+      role: role,
+      start_date: date,
+      rate: rate,
+      months_worked: monthsWorked,
+      total_bill: totalBill
+    };
+
+    firebase
+      .database()
+      .ref('/employee/new')
+      .push(newEmployee, function(error) {
+        if (error) {
+          console.log('error', error);
+
+          // The write failed...
+        } else {
+          // Data saved successfully!
+          console.log('data saved successfully!');
+        }
+      });
 
     var markup =
       '<tr><td>' +
@@ -25,7 +91,7 @@ $(document).ready(function() {
       rate +
       '</td><td>' +
       totalBill +
-      '$' +
+      ' $' +
       '</td></tr>';
 
     $('.employee-table tbody').append(markup);
